5e1b1009c8d523a0c1a116d8c29e337063c55031
busayo <prosperotemuyiwa@gmail.com>
2016-02-11 13:33:22 +0100

    feat(hackathon): Add Google authentication

--- config/services.php
+++ config/services.php
@@ -53,5 +53,11 @@ return [
         'redirect'      => env('GITHUB_CALLBACK_URL')
     ],
 
+    'google' => [
+        'client_id'     => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect'      => env('GOOGLE_CALLBACK_URL')
+    ],
+
 
 ];
