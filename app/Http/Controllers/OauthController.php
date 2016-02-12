fc55b837da793b6d7c457a2e605e484dc7bf4b1e
busayo <prosperotemuyiwa@gmail.com>
2016-02-12 23:43:58 +0100

    refactor(hackathon): Arrange imports from shortest to longest

--- app/Http/Controllers/OauthController.php
+++ app/Http/Controllers/OauthController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\Http\Requests;
-use App\Http\Controllers\Controller;
-use Illuminate\Contracts\Auth\Guard;
-use Laravel\Socialite\Contracts\Factory as Socialite;
 use App\User;
+use App\Http\Requests;
+use Illuminate\Contracts\Auth\Guard;
+use App\Http\Controllers\Controller;
+use Laravel\Socialite\Contracts\Factory as Socialite;
+
 
 class OauthController extends Controller
 {
