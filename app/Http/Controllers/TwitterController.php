d672d170f16af460c7cf29fecf9b32b7a54f2e42
busayo <prosperotemuyiwa@gmail.com>
2016-02-16 11:08:28 +0100

    chore(hackathon): Comment all things

--- app/Http/Controllers/TwitterController.php
+++ app/Http/Controllers/TwitterController.php
@@ -2,23 +2,31 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
-use App\Http\Requests;
-use App\Http\Controllers\Controller;
 use Twitter;
+use App\Http\Requests;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class TwitterController extends Controller
 {
+    /**
+     * Item to be searched for, combing through twitter's massive data
+     * @var string
+     */
     protected $searchItem;
-    protected $githubHandle;
 
+    /**
+     * Initialize the Controller with necessary arguments
+     */
     public function __construct()
     {
         $this->searchItem = 'laravel';
-        $this->githubHandle = 'unicodeveloper';
     }
 
+    /**
+     * Return all tweets to the Twitter API dashboard
+     * @return mixed
+     */
     public function getPage()
     {
         $searchedTweets = json_decode($this->searchForTweets($this->searchItem), true);
@@ -26,16 +34,30 @@ class TwitterController extends Controller
         return view('api.twitter')->withTweets($searchedTweets['statuses']);
     }
 
+    /**
+     * Get the latest tweets on a user timeline
+     * @return Collection
+     */
     private function getLatestTweets()
     {
         return Twitter::getHomeTimeline(['count' => 2, 'format' => 'json']);
     }
 
+    /**
+     * Search for tweets based on a search query
+     * @param  string $item
+     * @return Collection
+     */
     private function searchForTweets($item)
     {
         return Twitter::getSearch(['q' => $item, 'count' => 4, 'format' => 'json']);
     }
 
+    /**
+     * Post a tweet to the timeline
+     * @param  Request $request
+     * @return string
+     */
     public function sendTweet(Request $request)
     {
         $this->validate($request, [
