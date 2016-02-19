4f2aa4ced8565419de7a61284bc600950d15f04d
busayo <prosperotemuyiwa@gmail.com>
2016-02-19 06:56:22 +0100

    feat(hackathon): Add Lob API functionality

--- app/Http/Controllers/LobController.php
+++ app/Http/Controllers/LobController.php
@@ -4,17 +4,53 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Lob\Lob;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 class LobController extends Controller
 {
     /**
+     * LOB API KEY
+     * @var string
+     */
+    protected $apikey;
+
+    /**
+     * Instance of Lob
+     * @var object
+     */
+    protected $lob;
+
+    /**
+     * Initialize Lob
+     */
+    public function __construct()
+    {
+        $this->apikey = env('LOB_API_KEY');
+        $this->lob = new Lob($this->apikey);
+    }
+
+    /**
+     * Get all delivery routes for this zip code
+     * @param  string $zipcdode
+     * @return array
+     */
+    private function getRoutes($zipcode)
+    {
+        $results = $this->lob->routes()->all(['zip_codes' => $zipcode]);
+
+        return $results[0]['routes'];
+    }
+
+    /**
      * Return all data to the Lob API dashboard
      * @return mixed
      */
     public function getPage()
     {
-        return view('api.lob');
+        $routes = $this->getRoutes('10007');
+
+        return view('api.lob')->withRoutes($routes);
     }
 }
--- resources/views/api/lob.blade.php
+++ resources/views/api/lob.blade.php
@@ -9,11 +9,11 @@
         </div>
 
         <div class="btn-group btn-group-justified">
-            <a href="https://lob.com/docs" class="btn btn-primary">
+            <a href="https://lob.com/docs" target="_blank" class="btn btn-primary">
                 <i class="fa fa-check-square-o"></i> API Documentation
             </a>
-            <a href="https://github.com/lob/lob-node" target="_blank" class="btn btn-primary">
-                <i class="fa fa-code"></i> Lob Node Docs
+            <a href="https://github.com/lob/lob-php" target="_blank" class="btn btn-primary">
+                <i class="fa fa-code"></i> Lob PHP Docs
             </a>
             <a href="https://dashboard.lob.com/register" target="_blank" class="btn btn-primary">
                 <i class="fa fa-gear"></i> Create API Account
@@ -22,7 +22,27 @@
 
         <h3>Delivery routes in 10007</h3>
 
-        <table class="table table-striped table-bordered"><thead><tr><th>Route</th><th># of Residential Addresses</th><th># of Business Addresses</th></tr></thead><tbody><tr><td>B099</td><td>0</td><td>0</td></tr><tr><td>C000</td><td>0</td><td>0</td></tr><tr><td>C001</td><td>455</td><td>38</td></tr><tr><td>C003</td><td>13</td><td>12</td></tr><tr><td>C004</td><td>152</td><td>47</td></tr><tr><td>C005</td><td>0</td><td>34</td></tr><tr><td>C006</td><td>0</td><td>138</td></tr><tr><td>C008</td><td>254</td><td>93</td></tr><tr><td>C009</td><td>454</td><td>48</td></tr><tr><td>C012</td><td>239</td><td>46</td></tr><tr><td>C013</td><td>441</td><td>40</td></tr><tr><td>C014</td><td>0</td><td>23</td></tr><tr><td>C017</td><td>0</td><td>51</td></tr><tr><td>C018</td><td>0</td><td>157</td></tr><tr><td>C019</td><td>356</td><td>54</td></tr><tr><td>C020</td><td>1</td><td>122</td></tr><tr><td>C021</td><td>6</td><td>76</td></tr><tr><td>C022</td><td>400</td><td>62</td></tr><tr><td>C023</td><td>43</td><td>85</td></tr><tr><td>C098</td><td>440</td><td>29</td></tr><tr><td>C099</td><td>18</td><td>97</td></tr></tbody></table>
+         @if ($routes)
+            <table class="table table-striped table-bordered">
+                <thead>
+                    <tr>
+                        <th>Route</th>
+                        <th># of Residential Addresses</th>
+                        <th># of Business Addresses</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                @foreach ($routes as $route)
+                    <tr>
+                        <td>{{ $route['route'] }}</td>
+                        <td>{{ $route['residential'] }}</td>
+                        <td>{{ $route['business'] }}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        @endif
         <br>
     </div>
 @stop
