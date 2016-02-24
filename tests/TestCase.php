547961938bf3c739c1aaebc2fcd588a436b3bdfd
busayo <prosperotemuyiwa@gmail.com>
2016-02-24 07:28:36 +0100

    chore(tests): Add call to migrate data into sqlite

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -14,6 +14,23 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase
      *
      * @return \Illuminate\Foundation\Application
      */
+     public function setUp()
+     {
+        parent::setUp();
+        $this->prepareForTests();
+     }
+
+     public function prepareForTests()
+     {
+         Config::set('database.default', 'sqlite');
+         Artisan::call('migrate:refresh');
+     }
+
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
     public function createApplication()
     {
         $app = require __DIR__.'/../bootstrap/app.php';
