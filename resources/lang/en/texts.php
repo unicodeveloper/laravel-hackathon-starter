4dba23881831f37f7fedd6db8f7b403113f1fb97
Abu Baker Siddique <abskomol@gmail.com>
2016-06-06 17:31:15 +0600

    Language file added for common text.

---
+++ resources/lang/en/texts.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'message' => [
+        'sample_body' => 'Check out this message!',
+        'sent_success' => 'Your Message has been sent successfully',
+        'sent_failed' => 'Your Message could not be sent',
+    ],
+    'contact' => [
+        'subject' => 'Message From Laravel Hackathon Starter Contact Form',
+        'sent_success' => 'Your Message has been dispatched successfully',
+    ],
+    'twitter' => [
+        'success' => 'Your Tweet has been posted successfully',
+    ]
+];
