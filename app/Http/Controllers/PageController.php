8f502cf71a87cfe03800b8d59a8ee9975ccc9a1e
Abati Adewale <acekyd01@gmail.com>
2016-06-10 09:22:08 +0100

    quick fixes

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -1,24 +1,34 @@
 <?php
 
     namespace App\Http\Controllers;
-    use App\Http\Controllers\Controller;
-    use Illuminate\Http\Request;
 
     class PageController extends Controller
     {
         
+        /**
+         * Return the homepage view
+         * @return mixed
+         */   
         public function home()
         {
             return view('welcome');
 
         } 
 
+        /**
+         * Return the api dashboard view listing available apis
+         * @return mixed
+         */ 
         public function api()
         {
             return view('apidashboard');
 
         }    
 
+        /**
+         * Return the contact page view
+         * @return mixed
+         */ 
         public function contact()
         {
             return view('contact');
